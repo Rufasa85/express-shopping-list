489235bd570e67cbf2b5157744c07e3de899a442
Joe Rehfuss <joe.rehfuss@gmail.com>
2021-04-20 10:17:46 -0700

    basic restful routing done

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const PORT = 3000
+
+const app = express();
+
+let items=["cat food"];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+    res.send("welcome!")
+})
+
+app.get("/api/items",(req,res)=>{
+    res.json(items);
+})
+
+app.post("/api/items",(req,res)=>{
+    console.log(req.body.item);
+    items.push(req.body.item);
+    res.send(req.body.item)
+})
+
+app.delete("/api/items",(req,res)=>{
+    items = [];
+    res.send("list cleared!")
+})
+
+app.listen(PORT,()=>{
+    console.log('listening on port '+ PORT)
+})
